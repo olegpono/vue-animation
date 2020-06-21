9eff5c180b575da3d2996ee4f9c6c42f719a07bf
Oleg <ponochovnyi.oleg@gmail.com>
2020-06-21 20:06:12 +0300

    feat(page): added animation for the sixth section

--- components/sections/SeventhSection.vue
+++ components/sections/SeventhSection.vue
@@ -1,14 +1,12 @@
 <template>
   <section class="section section--purple" @inview="play">
-    <div class="container container--text-center">
-      <h2 v-animate="animationOption" class="section-title section-title--gray">
-        <div ref="splitAnimation" class="section-title__animation-container">
-          <span class="left">BADA</span>
-          <span class="right">BOOM</span>
-          <div class="line line--one"></div>
-          <div class="line line--two"></div>
-        </div>
-      </h2>
+    <div ref="container" class="container container--text-center">
+      <svg-icon
+        ref="scalableText"
+        v-animate="animationOption"
+        name="logo-text"
+        class="scalable-logo"
+      />
     </div>
   </section>
 </template>
@@ -20,11 +18,9 @@ export default {
     return {
       tl: null,
       animationOption: {
-        options: {
-          name: 'fade-in-up',
-          delay: 0.5,
-          duration: 1
-        }
+        name: 'fade-in',
+        delay: 1,
+        duration: 1
       }
     }
   },
@@ -42,35 +38,28 @@ export default {
       this.tl.play()
     },
     setAnimation() {
-      this.tl = new TimelineMax({ paused: true })
-      const textLeftEl = this.$refs.splitAnimation.querySelector('.left')
-      const textRigthEl = this.$refs.splitAnimation.querySelector('.right')
-      const lineEls = this.$refs.splitAnimation.querySelectorAll('.line')
-      const lineOneEl = this.$refs.splitAnimation.querySelector('.line--one')
-      const lineTwoEl = this.$refs.splitAnimation.querySelector('.line--two')
-      const offset = (textLeftEl.offsetWidth + textRigthEl.offsetWidth) / 12
-      const offsetHeight = textLeftEl.offsetHeight / 2
-      TweenMax.set(textLeftEl, { x: offset, opacity: 0 })
-      TweenMax.set(textRigthEl, { x: -offset, opacity: 0 })
-
-      const animationOptions = {
-        width: '100%',
-        opacity: 1,
-        delay: 0.5,
-        ease: Power2.out
+      const { scalableText, container } = this.$refs
+      const options = {
+        backgroundColor: '#3c3c3b',
+        ease: Power2.easeOut
       }
-
+      this.tl = new TimelineMax({
+        paused: true,
+        delay: 3,
+        onComplete: this.onComplete
+      })
       this.tl
-        .set(lineEls, { y: offsetHeight })
-        .staggerTo(lineEls, 1, animationOptions)
-        .to(lineOneEl, 0.5, { y: 0, ease: Power2.out })
-        .to(lineTwoEl, 0.5, { y: offsetHeight * 2, ease: Power2.out }, '-=0.5')
-        .staggerTo([textLeftEl, textRigthEl], 1.5, {
-          x: 0,
-          opacity: 1,
-          ease: Power2.out
-        })
-        .staggerTo(lineEls, 0.5, { opacity: 0 }, '-=0.5')
+        .to(scalableText, 2, { scale: 100, ease: Power4.easeIn })
+        .to(container, 1, options, '-=0.75')
+    },
+    onComplete() {
+      this.$root.$emit('go-next')
+      this.$root.$emit('setAllowScrolling', false)
+      setTimeout(() => {
+        this.tl.stop()
+        this.tl.progress(0)
+        this.$root.$emit('setAllowScrolling', true)
+      }, 3000)
     }
   }
 }
